0e19e721b685b889d6b07eb8eda60a3a5f4e449e
Michael <mstephenp@protonmail.com>
2021-04-17 12:18:42 -0500

    one more

--- elixir/pig-latin/lib/pig_latin.ex
+++ elixir/pig-latin/lib/pig_latin.ex
@@ -1,9 +1,5 @@
 defmodule PigLatin do
-
-  @vowels ~w(a e i o u yt xr x)
-  @consonant_group ~w(ch qu squ thr th sch)
-  @x_or_y ~w(x y)
-
+  @vowels ~w(a e i o u)
 
   @doc """
   Given a `phrase`, translate it a word at a time to Pig Latin.
@@ -21,83 +17,55 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(word) do
-
-    {first_letter, rest} = String.split_at(word, 1)
-
-    cond do
-      # String.contains?(word, ~w(ch qu squ thr th sch)) ->
-
-      first_letter in ~w(a e i o u x) ->
-        "#{word}ay"
-
-      true ->
-        "#{rest}#{first_letter}ay"
-    end
-
-
-    # translated_words = for word <- String.split(phrase) do
-    #   translate_word(word)
-    # end
-
-    # Enum.join(translated_words, " ")
-
+    String.split(word)
+    |> Enum.map(&translate_word(&1))
+    |> Enum.join(" ")
   end
 
+  @spec translate_word(String.t()) :: String.t()
   def translate_word(word) do
-    vowel_start = starts_with(@vowels, word)
-    cons_grp_start = starts_with(@consonant_group, word)
-    x_or_y_start = starts_with(@x_or_y, word) |> next_is_consonant()
-
     cond do
-      vowel_start ->
-        "#{word}ay"
-      cons_grp_start ->
+      String.starts_with?(word, ~w(x y)) && next_letter_is_consonant(word) ->
+        translate_as_vowel(word)
 
-        # {start, fin} = String.split_at(word, String.length(cons_grp_start))
-        {start, fin} = get_starting_consonants("", word)
-        "#{fin}#{start}ay"
+      String.contains?(word, ~w(yt xr)) ->
+        translate_as_vowel(word)
 
-      x_or_y_start ->
-        "#{word}ay"
+      String.first(word) in @vowels ->
+        translate_as_vowel(word)
 
-      !vowel_start ->
-        {start, fin} = get_starting_consonants("", word)
-        "#{fin}#{start}ay"
+      String.contains?(word, ~w(squ thr sch)) ->
+        {first_three, rest} = String.split_at(word, 3)
+        translate_as_consonant(first_three, rest)
+
+      String.contains?(word, ~w(ch qu th)) ->
+        {first_pair, rest} = String.split_at(word, 2)
+        translate_as_consonant(first_pair, rest)
 
       true ->
-        [start | fin] = String.split(word, "", trim: true)
-        "#{Enum.join(fin)}#{start}ay"
-      end
-  end
-
-
-  @spec starts_with([String.t()], String.t()) :: String.t | boolean()
-  def starts_with(group, phrase) do
-    Enum.find(group, false, &(String.starts_with?(phrase, &1)))
-  end
-
-  def next_is_consonant(letters) when not letters do
-    letters
-  end
-
-  def next_is_consonant(letters) do
-    !starts_with(@vowels, letters) or starts_with(@consonant_group, letters)
-  end
-
-  @spec get_starting_consonants(String.t(), String.t()) :: {String.t(), String.t()}
-  def get_starting_consonants(letters, word) do
-    IO.inspect letters
-    IO.inspect word
-    if !starts_with(@vowels, word) or starts_with(@consonant_group, word) do
-      if starts_with(@consonant_group, word) do
-        {first_letter, rest} = String.split_at(word, 2)
-        get_starting_consonants(letters <> first_letter, rest)
-      else
-        {first_letter, rest} = String.split_at(word, 1)
-        get_starting_consonants(letters <> first_letter, rest)
-      end
-    else
-      {letters, word}
+        {starting_consonants, rest} = split_at_first_vowel(word)
+        translate_as_consonant(starting_consonants, rest)
     end
   end
+
+  @spec split_at_first_vowel(String.t()) :: {String.t(), String.t()}
+  def split_at_first_vowel(word) do
+    [{_, index} | _] =
+      String.split(word, "", trim: true)
+      |> Enum.with_index()
+      |> Enum.filter(fn {c, _i} -> c in @vowels end)
+
+    String.split_at(word, index)
+  end
+
+  @spec next_letter_is_consonant(String.t()) :: boolean
+  def next_letter_is_consonant(word) do
+    String.at(word, 1) not in @vowels
+  end
+
+  @spec translate_as_vowel(String.t()) :: String.t()
+  def translate_as_vowel(word), do: "#{word}ay"
+
+  @spec translate_as_consonant(String.t(), String.t()) :: String.t()
+  def translate_as_consonant(letters, rest), do: "#{rest}#{letters}ay"
 end
