3526b1deaf929187f45c335b98ab42bc1bcfbe4b
Michael <mstephenp@protonmail.com>
2021-04-22 09:22:48 -0500

    updates

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -3,34 +3,32 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(number) when number == 0 do
+  def verse(0) do
     """
     No more bottles of beer on the wall, no more bottles of beer.
     Go to the store and buy some more, 99 bottles of beer on the wall.
     """
   end
 
-  def verse(number) when number == 1 do
+  def verse(1) do
     """
     1 bottle of beer on the wall, 1 bottle of beer.
     Take it down and pass it around, no more bottles of beer on the wall.
     """
   end
 
-  def verse(number) do
+  def verse(2) do
     """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottle#{pluralize?(number)} of beer on the wall.
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
     """
   end
 
-  @spec pluralize?(number()) :: String.t()
-  def pluralize?(number) do
-    if number > 2 do
-      "s"
-    else
-      ""
-    end
+  def verse(number) do
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
   end
 
   @doc """
@@ -39,12 +37,73 @@ defmodule BeerSong do
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range \\ 99..0) do
     for number <- range do
-      if number == 0 or number == Enum.min(range) do
+      if number == Enum.min(range) do
         verse(number)
       else
         verse(number) <> "\n"
       end
     end
-    |> Enum.join
+    |> Enum.join()
   end
+
+  # @spec verse(number) :: <<_::64, _::_*8>>
+  # def verse(number) do
+  #   to_string(amount(number)) <>
+  #     " bottle" <>
+  #     plural?(number) <>
+  #     " of beer on the wall, " <>
+  #     to_string(amount(number, last: true)) <>
+  #     " bottle" <>
+  #     plural?(number) <>
+  #     " of beer.\n" <>
+  #     take_or_store(number) <>
+  #     " bottle" <>
+  #     plural?(number, last: true) <>
+  #     " of beer on the wall.\n"
+  # end
+
+  # @spec amount(any, any) :: any
+  # def amount(number, last \\ false) do
+  #   cond do
+  #     number == 0 && last ->
+  #       "no more"
+
+  #     number == 0 ->
+  #       "No more"
+
+  #     true ->
+  #       number
+  #   end
+  # end
+
+  # @spec take_or_store(number()) :: String.t()
+  # def take_or_store(number) do
+  #   cond do
+  #     number == 0 ->
+  #       "Go to the store and buy some more, 99"
+
+  #     number == 1 ->
+  #       "Take it down and pass it around, no more"
+
+  #     true ->
+  #       "Take one down and pass it around, #{number - 1}"
+  #   end
+  # end
+
+  # @spec plural?(any, any) :: binary
+  # def plural?(number, last \\ false) do
+  #   cond do
+  #     number == 2 and last ->
+  #       ""
+
+  #     number == 1 and last ->
+  #       "s"
+
+  #     number > 1 or number == 0 ->
+  #       "s"
+
+  #     true ->
+  #       ""
+  #   end
+  # end
 end
