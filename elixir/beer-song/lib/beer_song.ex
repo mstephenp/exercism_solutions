85c70ffe9d9b9f7c3fd32b8fb3d78707168893e6
Michael <mstephenp@protonmail.com>
2021-04-22 09:36:13 -0500

    another update

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -38,72 +38,72 @@ defmodule BeerSong do
   def lyrics(range \\ 99..0) do
     for number <- range do
       if number == Enum.min(range) do
-        verse(number)
+        verse_v2(number)
       else
-        verse(number) <> "\n"
+        verse_v2(number) <> "\n"
       end
     end
     |> Enum.join()
   end
 
-  # @spec verse(number) :: <<_::64, _::_*8>>
-  # def verse(number) do
-  #   to_string(amount(number)) <>
-  #     " bottle" <>
-  #     plural?(number) <>
-  #     " of beer on the wall, " <>
-  #     to_string(amount(number, last: true)) <>
-  #     " bottle" <>
-  #     plural?(number) <>
-  #     " of beer.\n" <>
-  #     take_or_store(number) <>
-  #     " bottle" <>
-  #     plural?(number, last: true) <>
-  #     " of beer on the wall.\n"
-  # end
+  @spec verse_v2(number) :: <<_::64, _::_*8>>
+  def verse_v2(number) do
+    to_string(amount(number)) <>
+      " bottle" <>
+      plural?(number) <>
+      " of beer on the wall, " <>
+      to_string(amount(number, last: true)) <>
+      " bottle" <>
+      plural?(number) <>
+      " of beer.\n" <>
+      take_or_store(number) <>
+      " bottle" <>
+      plural?(number, last: true) <>
+      " of beer on the wall.\n"
+  end
 
-  # @spec amount(any, any) :: any
-  # def amount(number, last \\ false) do
-  #   cond do
-  #     number == 0 && last ->
-  #       "no more"
+  @spec amount(any, any) :: any
+  def amount(number, last \\ false) do
+    cond do
+      number == 0 && last ->
+        "no more"
 
-  #     number == 0 ->
-  #       "No more"
+      number == 0 ->
+        "No more"
 
-  #     true ->
-  #       number
-  #   end
-  # end
+      true ->
+        number
+    end
+  end
 
-  # @spec take_or_store(number()) :: String.t()
-  # def take_or_store(number) do
-  #   cond do
-  #     number == 0 ->
-  #       "Go to the store and buy some more, 99"
+  @spec take_or_store(number()) :: String.t()
+  def take_or_store(number) do
+    cond do
+      number == 0 ->
+        "Go to the store and buy some more, 99"
 
-  #     number == 1 ->
-  #       "Take it down and pass it around, no more"
+      number == 1 ->
+        "Take it down and pass it around, no more"
 
-  #     true ->
-  #       "Take one down and pass it around, #{number - 1}"
-  #   end
-  # end
+      true ->
+        "Take one down and pass it around, #{number - 1}"
+    end
+  end
 
-  # @spec plural?(any, any) :: binary
-  # def plural?(number, last \\ false) do
-  #   cond do
-  #     number == 2 and last ->
-  #       ""
+  @spec plural?(any, any) :: binary
+  def plural?(number, last \\ false) do
+    cond do
+      number == 2 and last ->
+        ""
 
-  #     number == 1 and last ->
-  #       "s"
+      number == 1 and last ->
+        "s"
 
-  #     number > 1 or number == 0 ->
-  #       "s"
+      number > 1 or number == 0 ->
+        "s"
 
-  #     true ->
-  #       ""
-  #   end
-  # end
+      true ->
+        ""
+    end
+  end
 end
